f5004fc023cf2abd6af1dd5edf6c6a37443addc8
PASUPUREDDY VAMSI KRISHNA <163725254+2300031005@users.noreply.github.com>
2024-12-08 06:49:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+import Search from './pages/Search';
+
+export default function App() {
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_BACKEND_URL;
+
+    fetch(`${apiUrl}/api/some-route`)
+      .then((res) => res.json())
+      .then((data) => console.log('Data from backend:', data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
